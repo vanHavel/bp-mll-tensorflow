96531e7a26f118ea3e54944f39604f99d9cb59aa
Lukas <lukashuwald@online.de>
2017-08-16 17:44:40 +0200

    bp-mll-implementation and test

---
+++ bp_mll_test.py
@@ -0,0 +1,21 @@
+import numpy as np
+import tensorflow as tf
+
+from bp_mll import bp_mll_loss
+
+# 2 samples, 4 possible labels
+y_true = np.asarray([[ 1, -1, -1,  1], 
+                     [-1, -1,  1, -1]],
+                    dtype='float32')
+          
+# predictions for the samples        
+y_pred = np.asarray([[0.8, 0.3, 0.1, 0.9], 
+                     [0.1, 0.0, 1.0, 0.5]],
+                    dtype='float32')
+
+# compute result in tensorflow
+sess = tf.Session()
+result = sess.run(bp_mll_loss(y_true, y_pred))
+
+# should print 0.985641
+print(result)
